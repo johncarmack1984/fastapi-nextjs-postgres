1810ff965136ae31eebae19bee695545da93b825
John Carmack <johncarmack@me.com>
2024-03-27 11:30:51 -0700

    Comment out generateStaticParams function

--- app/community/post/[post_url]/page.tsx
+++ app/community/post/[post_url]/page.tsx
@@ -4,16 +4,16 @@ import db from "@/lib/db";
 import { SelectPostSchema, selectPostSchema } from "@/lib/validate/posts";
 import Post from "./post";
 
-export async function generateStaticParams() {
-  const post_urls = await db
-    .selectFrom("auxhealth_posts")
-    .select(["post_url"])
-    .execute();
+// export async function generateStaticParams() {
+//   const post_urls = await db
+//     .selectFrom("auxhealth_posts")
+//     .select(["post_url"])
+//     .execute();
 
-  return post_urls.slice(0, 10).map(({ post_url }) => ({
-    post_url,
-  }));
-}
+//   return post_urls.slice(0, 10).map(({ post_url }) => ({
+//     post_url,
+//   }));
+// }
 
 export default async function PostPage({
   params: { post_url },
